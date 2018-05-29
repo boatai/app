64ee2f4c883422e86adceac6a2bee866c8da2572
robintreur <robintreur@gmail.com>
2018-05-29 16:28:17 +0200

    Added check if appbar is collapsed in DetailActivity

--- app/src/main/java/com/boat/app/boatapp/DetailActivity.java
+++ app/src/main/java/com/boat/app/boatapp/DetailActivity.java
@@ -8,7 +8,9 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -62,10 +64,23 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
                         .setAction("Action", null).show();
             }
         });
+
         //collabsible toolbar
         collapsingToolbarLayout = findViewById(R.id.toolbar_layout);
-
         collapsingToolbarLayout.setTitle(this.namePackages);
+
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(final AppBarLayout appBarLayout, final int verticalOffset) {
+                float scrolling = (appBarLayout.getY() + appBarLayout.getTotalScrollRange());
+                if(scrolling <= 0){
+                    Log.d("SCROLLING", "AppBar is collapsed");
+                }else{
+                    Log.d("SCROLLING", "AppBar is not collapsed");
+                }
+            }
+        });
 
     }
 
